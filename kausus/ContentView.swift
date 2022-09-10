cf017e4cfe32b4ce7d4322aacbb391f7e25da705
Ilja Stepanow <ghyston@gmail.com>
2022-09-10 21:55:41 +0200

    Initial Commit

---
+++ kausus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kausus
+//
+//  Created by Ilja Stepanow on 10.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kausus/kaususApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaususApp.swift
+//  kausus
+//
+//  Created by Ilja Stepanow on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct kaususApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
